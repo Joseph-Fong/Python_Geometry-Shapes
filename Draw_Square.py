9581056038ee12f3ac12100c36f85d7019e46b78
Joseph Fong <111833444+Joseph-Fong@users.noreply.github.com>
2024-01-04 17:20:27 -0500

    Add files via upload

---
+++ Draw_Square.py
@@ -0,0 +1,11 @@
+import turtle
+t = turtle.Pen()
+t.down
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.up
---
+++ Draw_Triangle.py
@@ -0,0 +1,9 @@
+import turtle
+t = turtle.Pen()
+t.down
+t.forward(50)
+t.right(120)
+t.forward(50)
+t.right(120)
+t.forward(50)
+t.up
